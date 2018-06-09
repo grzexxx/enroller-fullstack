82a9eb21674ef85d7bc031461a46e07b34829d74
Gjahn <gjahn100@gmail.com>
2018-06-09 14:24:41 +0200

    add working registration

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -48,6 +48,15 @@
             },
             logout() {
                 this.authenticatedUsername = '';
+            },
+            register(user) {
+                this.$http.post('participants', user)
+                    .then(response => {
+                        // udało się
+                    })
+                    .catch(response => {
+                        // nie udało sie
+                    });
             }
         }
     };
--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -4,6 +4,8 @@ import VueResource from "vue-resource"
 
 Vue.use(VueResource);
 
+Vue.http.options.root = '/api';
+
 new Vue({
     el: '#app',
     render: h => h(App)
